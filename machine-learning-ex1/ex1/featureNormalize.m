8d6d76457460f595e11e6a58366cff260cb9c8d3
Sergey Zarochentsev <zsergey26@gmail.com>
2015-12-09 23:21:38 +0200

    Ex 1 complete

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -27,7 +27,7 @@ sigma = zeros(1, size(X, 2));
 %       
 
 
-
+X_norm = (X - mean(X))/ std(X);
 
 
 
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -7,6 +7,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -19,8 +20,8 @@ for iter = 1:num_iters
 
 
 
-    s = 1/m * sum(X*theta-y)*X
-    theta = theta  - alpha * s; 
+    s = 1/m * (X*theta - y)' * X;
+    theta = theta  - alpha * s'; 
 
 
     % ============================================================
